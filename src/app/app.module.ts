c5a3f015b10125381b8ce5b66b50c30d92399be7
Jhoan Sebastian Agudelo Sierra <jsagudelo@magnum.com.co>
2018-08-20 20:04:55 -0500

    Se implementa el manejo de rutas del portafolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+/**Modulos */
+import {AppRoutingModule} from './app.routing.module';
+
+
+/**Componentes */
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { RouterModule } from '../../node_modules/@angular/router';
 
 
 @NgModule({
@@ -13,11 +22,15 @@ import { PortafolioComponent } from './pages/portafolio/portafolio.component';
     HeaderComponent,
     FooterComponent,
     PortafolioComponent,
+    AboutComponent,
+    ItemComponent,
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,AppRoutingModule
+
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
